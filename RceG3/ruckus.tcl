5a0fb7341b6298c5c5280fee3db175f40a2a2c27
Larry Ruckman <ruckman@slac.stanford.edu>
2018-04-06 09:32:30 -0700

    adding Vivado version checking and submodule checking

--- RceG3/ruckus.tcl
+++ RceG3/ruckus.tcl
@@ -6,3 +6,10 @@ loadSource -dir  "$::DIR_PATH/hdl/"
 loadSource -path "$::DIR_PATH/simlink/rtl/RceG3CpuSim.vhd"
 loadIpCore -dir  "$::DIR_PATH/coregen/processing_system7_0/"
 loadSource -sim_only -path "$::DIR_PATH/tb/rceg3_tb.vhd"
+
+# Check for submodule tagging
+if { [SubmoduleCheck {ruckus} {1.6.3} ] < 0 } {exit -1}
+if { [SubmoduleCheck {surf}   {1.7.4} ] < 0 } {exit -1}
+
+# Check for version 2016.4 of Vivado (or later)
+if { [VersionCheck 2016.4] < 0 } {exit -1}
