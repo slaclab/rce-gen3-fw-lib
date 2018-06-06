eea3deec71d2aa39f280346bffa097ff78586791
Larry Ruckman <ruckman@slac.stanford.edu>
2018-06-05 17:05:40 -0700

    updating to rogue v2

--- python/RceG3/RceVersion.py
+++ python/RceG3/RceVersion.py
@@ -18,47 +18,186 @@ import pyrogue as pr
 import collections
 
 class RceVersion(pr.Device):
-    def __init__(self, **kwargs):
-        super(self.__class__, self).__init__(
-            description="RCE Version and BSI register.", **kwargs)
+    def __init__(
+            self,       
+            name        = 'RceVersion',
+            description = 'Container for RceVersion Module',
+            offset      = 0x00000000,
+            **kwargs):
+    
+        super().__init__(
+            name        = name,
+            description = description,
+            offset      = 0x00000000, #This module using absolute address (not offsets)
+            **kwargs)
 
-        self.add(pyrogue.Variable(name='fpgaVersion', description='Fpga firmware version number',
-                                  offset=0x80000000, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'FpgaVersion', 
+            description = 'Fpga firmware version number',
+            offset      = 0x80000000, 
+            mode        = 'RO',
+        ))
 
-        self.add(pyrogue.Variable(name='scratchPad', description='Scratchpad Register',
-                                  offset=0x80000004, bitsize=32, bitoffset=0, base='hex', mode='RW'))
+        self.add(pr.RemoteVariable(
+            name        = 'ScratchPad', 
+            description = 'Scratchpad Register',
+            offset      = 0x80000004, 
+            mode        = 'RW',
+        ))
 
-        self.add(pyrogue.Variable(name='rceVersion', description='RCE registers version number',
-                                  offset=0x80000008, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'RceVersion', 
+            description = 'RCE registers version number',
+            offset      = 0x80000008, 
+            mode='RO',
+        ))
 
-        self.add(pyrogue.Variable(name='deviceDna', description='Xilinx Device DNA Value',
-                                  offset=0x80000020, bitsize=64, bitoffset=0, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'DeviceDna', 
+            description = 'Xilinx Device DNA Value',
+            offset      = 0x80000020, 
+            bitsize     = 64, 
+            mode        = 'RO',
+        ))
 
-        self.add(pyrogue.Variable(name='eFuseValue', description='Xilinx E-Fuse Value',
-                                  offset=0x80000030, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'EFuseValue', 
+            description = 'Xilinx E-Fuse Value',
+            offset      = 0x80000030, 
+            mode        = 'RO',
+        ))
 
-        self.add(pyrogue.Variable(name='ethMode', description='Ethernet Mode',
-                                  offset=0x80000034, bitsize=32, bitoffset=0, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'EthMode', 
+            description = 'Ethernet Mode',
+            offset      = 0x80000034, 
+            mode        = 'RO',
+        ))
 
-        self.add(pyrogue.Variable(name='heartBeat', description='A constantly incrementing value',
-                                  offset=0x80000038, bitsize=32, bitoffset=0, 
-                                  pollInterval=1, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'HeartBeat', 
+            description = 'A constantly incrementing value',
+            offset      = 0x80000038, 
+            mode        = 'RO',
+            pollInterval= 1, 
+        ))
+        
+        self.add(pr.RemoteVariable(   
+            name         = 'GitHash',
+            description  = 'GIT SHA-1 Hash',
+            offset       = 0x80000040,
+            bitSize      = 160,
+            bitOffset    = 0,
+            base         = pr.UInt,
+            mode         = 'RO',
+            hidden       = True,
+        ))
 
-        self.add(pyrogue.Variable(name='gitHash', description='GIT SHA-1 Hash',
-                                 offset=0x80000040, bitSize=160, bitOffset=0, base='hex', mode='RO'))
+        self.add(pr.LinkVariable(
+            name         = 'GitHashShort',
+            mode         = 'RO',
+            dependencies = [self.GitHash],
+            disp         = '{:07x}',
+            linkedGet    = lambda: self.GitHash.value() >> 132
+        ))        
+        
+        self.add(pr.RemoteVariable(   
+            name         = 'BuildStamp',
+            description  = 'Firmware Build String',
+            offset       = 0x80001000,
+            bitSize      = 8*256,
+            base         = pr.String,
+            mode         = 'RO',
+            hidden       = True,
+        ))        
 
-        self.add(pyrogue.Variable(name='buildStamp', description='Firmware build string',
-                                 offset=0x80001000, bitSize=256*8, bitOffset=0, base='string', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'SerialNumber', 
+            description = 'Serial Number',
+            offset      = 0x84000140, 
+            bitSize     = 64, 
+            mode        = 'RO',
+        ))
 
-        self.add(pyrogue.Variable(name='serialNumber', description='Serial Number',
-                                 offset=0x84000140, bitSize=64, bitOffset=0, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'AtcaSlot', 
+            description = 'ATCA Slot',
+            offset      = 0x84000148, 
+            bitSize     = 8, 
+            bitOffset   = 16, 
+            mode        = 'RO',
+        ))
 
-        self.add(pyrogue.Variable(name='atcaSlot', description='ATCA Slot',
-                                  offset=0x84000148, bitSize=8, bitOffset=16, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'CobBay', 
+            description = 'COB Bay',
+            offset      = 0x84000148, 
+            bitSize     = 8, 
+            bitOffset   = 8, 
+            mode        = 'RO',
+        ))
 
-        self.add(pyrogue.Variable(name='cobBay', description='COB Bay',
-                                  offset=0x84000148, bitSize=8, bitOffset=8, base='hex', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name        = 'CobElement', 
+            description = 'COB Element',
+            offset      = 0x84000148, 
+            bitSize     = 8, 
+            mode        = 'RO',
+        ))
+        
+        def parseBuildStamp(var, value, disp):
+            p = parse.parse("{ImageName}: {BuildEnv}, {BuildServer}, Built {BuildDate} by {Builder}", value.strip())
+            if p is not None:
+                for k,v in p.named.items():
+                    self.node(k).set(v)
+        
+        self.add(pr.LocalVariable(
+            name = 'ImageName',
+            mode = 'RO',
+            value = ''))
+ 
+        self.add(pr.LocalVariable(
+            name = 'BuildEnv',
+            mode = 'RO',
+            value = ''))
 
-        self.add(pyrogue.Variable(name='cobElement', description='COB Element',
-                                  offset=0x84000148, bitSize=8, bitOffset=0, base='hex', mode='RO'))
+        self.add(pr.LocalVariable(
+            name = 'BuildServer',
+            mode = 'RO',
+            value = ''))
+       
+        self.add(pr.LocalVariable(
+            name = 'BuildDate',
+            mode = 'RO',
+            value = ''))
+       
+        self.add(pr.LocalVariable(
+            name = 'Builder',
+            mode = 'RO',
+            value = ''))
 
+        self.BuildStamp.addListener(parseBuildStamp)      
+
+    def hardReset(self):
+        print('RceVersion hard reset called')
+
+    def softReset(self):
+        print('RceVersion soft reset called')
+
+    def countReset(self):
+        print('RceVersion count reset called')
+        
+    def printStatus(self):
+        self.BuildStamp.get()
+        gitHash = self.GitHash.get()
+        print("FwVersion    = {}".format(hex(self.FpgaVersion.get())))
+        if (gitHash != 0):
+            print("GitHash      = {}".format(hex(self.GitHash.get())))
+        else:
+            print("GitHash      = dirty (uncommitted code)")
+        print("FwTarget     = {}".format(self.ImageName.get()))
+        print("BuildEnv     = {}".format(self.BuildEnv.get()))
+        print("BuildServer  = {}".format(self.BuildServer.get()))
+        print("BuildDate    = {}".format(self.BuildDate.get()))
+        print("Builder      = {}".format(self.Builder.get()))        
+        
