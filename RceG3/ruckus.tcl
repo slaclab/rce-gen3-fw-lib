e41cdf392784090e261086af831a75e9c7bccd6f
Larry Ruckman <ruckman@slac.stanford.edu>
2018-06-14 10:15:44 -0700

    reorg of source code

--- RceG3/ruckus.tcl
+++ RceG3/ruckus.tcl
@@ -2,7 +2,8 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load local Source Code
-loadSource -dir  "$::DIR_PATH/hdl/"
+loadSource -dir  "$::DIR_PATH/hdl"
+loadSource -dir  "$::DIR_PATH/hdl/common"
 loadSource -path "$::DIR_PATH/simlink/rtl/RceG3CpuSim.vhd"
 loadIpCore -dir  "$::DIR_PATH/coregen/processing_system7_0/"
 loadSource -sim_only -path "$::DIR_PATH/tb/rceg3_tb.vhd"
