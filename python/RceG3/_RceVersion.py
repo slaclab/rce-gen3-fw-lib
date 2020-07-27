c445290d6731888ef3d3a9359ed2d6adb1946b4b
Benjamin Reese <bareese@slac.stanford.edu>
2020-07-27 15:23:08 -0700

    Fix whitespace

--- python/RceG3/_RceVersion.py
+++ python/RceG3/_RceVersion.py
@@ -1,12 +1,12 @@
 #-----------------------------------------------------------------------------
 # Title      : RCE Gen3 Version Register space
 #-----------------------------------------------------------------------------
-# This file is part of the RCE GEN3 firmware platform. It is subject to 
-# the license terms in the LICENSE.txt file found in the top-level directory 
-# of this distribution and at: 
-#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
-# No part of the rogue RCE GEN3 firmware platform, including this file, may be 
-# copied, modified, propagated, or distributed except according to the terms 
+# This file is part of the RCE GEN3 firmware platform. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the rogue RCE GEN3 firmware platform, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
 
@@ -22,64 +22,64 @@ class RceVersion(pr.Device):
             intOffset   = 0x80000000, # Internal registers offset (zynq=0x80000000, zynquplus=0xB0000000)
             bsiOffset   = 0x84000000, # BSI I2C Slave Registers   (zynq=0x84000000, zynquplus=0xB0010000)
             **kwargs):
-    
+
         super().__init__(
             description = description,
             offset      = 0x00000000, #This module using absolute address (not offsets)
             **kwargs)
 
         self.add(pr.RemoteVariable(
-            name        = 'FpgaVersion', 
+            name        = 'FpgaVersion',
             description = 'Fpga firmware version number',
-            offset      = intOffset+0x0, 
+            offset      = intOffset+0x0,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'ScratchPad', 
+            name        = 'ScratchPad',
             description = 'Scratchpad Register',
-            offset      = intOffset+0x4, 
+            offset      = intOffset+0x4,
             mode        = 'RW',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'RceVersion', 
+            name        = 'RceVersion',
             description = 'RCE registers version number',
-            offset      = intOffset+0x8, 
+            offset      = intOffset+0x8,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'DeviceDna', 
+            name        = 'DeviceDna',
             description = 'Xilinx Device DNA Value',
-            offset      = intOffset+0x20, 
-            bitSize     = 64, 
+            offset      = intOffset+0x20,
+            bitSize     = 64,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'EFuseValue', 
+            name        = 'EFuseValue',
             description = 'Xilinx E-Fuse Value',
-            offset      = intOffset+0x30, 
+            offset      = intOffset+0x30,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'EthMode', 
+            name        = 'EthMode',
             description = 'Ethernet Mode',
-            offset      = intOffset+0x34, 
+            offset      = intOffset+0x34,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'HeartBeat', 
+            name        = 'HeartBeat',
             description = 'A constantly incrementing value',
-            offset      = intOffset+0x38, 
+            offset      = intOffset+0x38,
             mode        = 'RO',
-            pollInterval= 1, 
+            pollInterval= 1,
         ))
-        
-        self.add(pr.RemoteVariable(   
+
+        self.add(pr.RemoteVariable(
             name         = 'GitHash',
             description  = 'GIT SHA-1 Hash',
             offset       = intOffset+0x40,
@@ -95,9 +95,9 @@ class RceVersion(pr.Device):
             mode         = 'RO',
             dependencies = [self.GitHash],
             linkedGet    = lambda read: f'{(self.GitHash.get(read=read) >> 132):x}'
-        ))        
-        
-        self.add(pr.RemoteVariable(   
+        ))
+
+        self.add(pr.RemoteVariable(
             name         = 'BuildStamp',
             description  = 'Firmware Build String',
             offset       = intOffset+0x1000,
@@ -105,49 +105,49 @@ class RceVersion(pr.Device):
             base         = pr.String,
             mode         = 'RO',
             hidden       = True,
-        ))        
+        ))
 
         self.add(pr.RemoteVariable(
-            name        = 'SerialNumber', 
+            name        = 'SerialNumber',
             description = 'Serial Number',
-            offset      = bsiOffset+0x140, 
-            bitSize     = 64, 
+            offset      = bsiOffset+0x140,
+            bitSize     = 64,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'AtcaSlot', 
+            name        = 'AtcaSlot',
             description = 'ATCA Slot',
-            offset      = bsiOffset+0x148, 
-            bitSize     = 8, 
-            bitOffset   = 16, 
+            offset      = bsiOffset+0x148,
+            bitSize     = 8,
+            bitOffset   = 16,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'CobBay', 
+            name        = 'CobBay',
             description = 'COB Bay',
-            offset      = bsiOffset+0x148, 
-            bitSize     = 8, 
-            bitOffset   = 8, 
+            offset      = bsiOffset+0x148,
+            bitSize     = 8,
+            bitOffset   = 8,
             mode        = 'RO',
         ))
 
         self.add(pr.RemoteVariable(
-            name        = 'CobElement', 
+            name        = 'CobElement',
             description = 'COB Element',
-            offset      = bsiOffset+0x148, 
-            bitSize     = 8, 
+            offset      = bsiOffset+0x148,
+            bitSize     = 8,
             mode        = 'RO',
         ))
-        
+
         def parseBuildStamp(var, read):
             p = parse.parse("{ImageName}: {BuildEnv}, {BuildServer}, Built {BuildDate} by {Builder}", var.dependencies[0].get(read))
             if p is None:
                 return ''
             else:
                 return p[var.name]
-        
+
         self.add(pr.LinkVariable(
             name = 'ImageName',
             mode = 'RO',
@@ -165,13 +165,13 @@ class RceVersion(pr.Device):
             mode = 'RO',
             linkedGet = parseBuildStamp,
             variable = self.BuildStamp))
-       
+
         self.add(pr.LinkVariable(
             name = 'BuildDate',
             mode = 'RO',
             linkedGet = parseBuildStamp,
             variable = self.BuildStamp))
-       
+
         self.add(pr.LinkVariable(
             name = 'Builder',
             mode = 'RO',
@@ -187,7 +187,7 @@ class RceVersion(pr.Device):
 
     def countReset(self):
         print('RceVersion count reset called')
-        
+
     def printStatus(self):
         self.BuildStamp.get()
         gitHash = self.GitHash.get()
@@ -200,5 +200,4 @@ class RceVersion(pr.Device):
         print("BuildEnv     = {}".format(self.BuildEnv.get()))
         print("BuildServer  = {}".format(self.BuildServer.get()))
         print("BuildDate    = {}".format(self.BuildDate.get()))
-        print("Builder      = {}".format(self.Builder.get()))        
-        
+        print("Builder      = {}".format(self.Builder.get()))
